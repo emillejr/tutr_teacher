83e3374224d9c13cc6641e3bc93d03a7abeed83e
emillejunior <emille.junior@gmail.com>
2019-02-25 20:11:59 +0700

    25feb2019

--- database/seeds/StudentsTableSeeder.php
+++ database/seeds/StudentsTableSeeder.php
@@ -71,5 +71,77 @@ class StudentsTableSeeder extends Seeder
             'password' => '$2y$10$HdgnXmJuB2aFE/iysHhV4us.dFt2hqMkRRRMxwbqYlE4F1tAbHt3e',
             'institution_id' => '1'
         ]);
+
+
+        DB::table('student_profiles')->insert([
+            'id' => '1',
+            'user_id' => '1',
+            'first_name' => 'Zidane',
+            'last_name' => 'Prabowo',
+            'gender' => 'L'
+        ]);
+        DB::table('student_profiles')->insert([
+            'id' => '2',
+            'user_id' => '2',
+            'first_name' => 'Jessica',
+            'last_name' => 'Subiakto',
+            'gender' => 'P'
+        ]);
+        DB::table('student_profiles')->insert([
+            'id' => '3',
+            'user_id' => '3',
+            'first_name' => 'Stuart',
+            'last_name' => 'Kuncorodiningrat',
+            'gender' => 'L'
+        ]);
+        DB::table('student_profiles')->insert([
+            'id' => '4',
+            'user_id' => '4',
+            'first_name' => 'Nayla',
+            'last_name' => 'Subiakto',
+            'gender' => 'P'
+        ]);
+        DB::table('student_profiles')->insert([
+            'id' => '5',
+            'user_id' => '5',
+            'first_name' => 'Billy',
+            'last_name' => 'Sukamsih',
+            'gender' => 'L'
+        ]);
+        DB::table('student_profiles')->insert([
+            'id' => '6',
+            'user_id' => '6',
+            'first_name' => 'Cayla',
+            'last_name' => 'Hardibudi',
+            'gender' => 'P'
+        ]);
+        DB::table('student_profiles')->insert([
+            'id' => '7',
+            'user_id' => '7',
+            'first_name' => 'Winda',
+            'last_name' => 'Yulianto',
+            'gender' => 'P'
+        ]);
+        DB::table('student_profiles')->insert([
+            'id' => '8',
+            'user_id' => '8',
+            'first_name' => 'Michael',
+            'last_name' => 'Fitrianasari',
+            'gender' => 'L'
+        ]);
+        DB::table('student_profiles')->insert([
+            'id' => '9',
+            'user_id' => '9',
+            'first_name' => 'Bobby',
+            'last_name' => 'Lisnawati',
+            'gender' => 'L'
+        ]);
+        DB::table('student_profiles')->insert([
+            'id' => '10',
+            'user_id' => '10',
+            'first_name' => 'Richard',
+            'last_name' => 'Salim',
+            'gender' => 'L'
+        ]);
     }
 }
